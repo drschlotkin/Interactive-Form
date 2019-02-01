2b7db771821e2045ecaaf42a3ed5ec7cda0a5ecd
Ewen Earle <ewen_earle@hotmail.com>
2019-02-01 14:01:06 -0330

    third draft

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// Hide elements on start up
-$('#colors-js-puns').hide();
-const other = $('#other-title').detach()
-$('p').hide();
-$('#name').focus();
-$('#payment').val('credit card')
-$("#payment option[value='select_method']").attr("disabled", true)
-
-
-// Display 'Job Role: Other' element
-$('#title').on('change',  () => {
-    $('#title').val() === 'other' ? $(other).insertAfter('#title') : $('#other-title').hide();
-})
-
-
 // Variables for registration form
 let totalCost = 0;
 const main = "input[name='all']";
@@ -24,11 +9,36 @@ const build = "input[name='build-tools']";
 const npm = "input[name='npm']";
 
 
+// Hide elements and assign default values/attributes
+$('#colors-js-puns').hide();
+$('#other-title').hide()
+$('#name').focus();
+$('#payment').val('credit card');
+$("#payment option[value='select_method']").attr("disabled", true);
+
+
+// Display 'Job Role: Other' element
+$('#title').on('change',  () => $('#title').val() === 'other' ? $('#other-title').show() : $('#other-title').hide());
+
+
+// Email validation
+$('#mail').on('keyup', () => {
+    let regex = /^[^@]+@[^@.]+\.[a-z]+$/i;
+    if (regex.test($('#mail').val()) || $('#mail').val() == ''){
+        $('#mail').prev().text('Email:').css('color', 'black');
+    }else{
+        $('#mail').prev().text('Please enter a valid email address:').css('color', '#e60000');
+       
+    };
+});
+
+
 /*===============
 T-Shirt Selection
 =================*/
 
 $("#design").on('change', () => {
+
     // Remove 'Select Theme' selection   
     let design = $('#design').find("option").eq(0);
     if (design.val() == 'Select Theme') design.remove();
@@ -37,9 +47,9 @@ $("#design").on('change', () => {
     if ($('#design').val() == 'js puns'){
         $('#colors-js-puns').show();
         $('#color').val('cornflowerblue');
-            for (let i = 0; i <=5; i++){
-                (i < 3) ? $(`#color :eq(${i})`).show() : $(`#color :eq(${i})`).hide();
-            };
+        for (let i = 0; i <=5; i++){
+            (i < 3) ? $(`#color :eq(${i})`).show() : $(`#color :eq(${i})`).hide();
+        };
     }else{
         $('#colors-js-puns').show();
         $('#color').val('tomato');
@@ -52,9 +62,9 @@ $("#design").on('change', () => {
 
 /*=====================
 Activities Registration
-=======================
-DISABLE/ENABLE ACTIVITIES BASED ON THE TIME SLOTS AND CALCULATE TOTAL COST */
-
+=======================*/
+ 
+// Disable or enable activities based on the time slots and calculate total cost
 $(main).on('click', () => $(main).is(':checked') ? add(200) : sub(200));
 $(frameworks).on('click', () => $(frameworks).is(':checked') ? addCost(express, 100) : subCost(express, 100));
 $(libraries).on('click', () => $(libraries).is(':checked') ? addCost(node, 100): subCost(node, 100));
@@ -72,7 +82,7 @@ const addCost = (checkbox, cost) => {
 // If activity is 'unchecked', subtract from total cost and enable fields
 const subCost = (checkbox, cost) => {
     sub(cost);
-    $(checkbox).prop('disabled', false).parent().css('color', 'black').css('cursor', 'pointer');
+    $(checkbox).prop('disabled', false).css('cursor', 'pointer').parent().css('color', 'black');
 };
 
 // Addition function
@@ -95,7 +105,6 @@ Payment Info
 =============*/
 
 $('#payment').on('change', () => {
-
    
     // Display payment info based on selection 
     if ($('#payment').val() == 'credit card'){
@@ -105,82 +114,89 @@ $('#payment').on('change', () => {
     } 
 
     if ($('#payment').val() == 'paypal') {
+        eraseCreditInfo()
         $('#credit-card').hide();
         $('div:last-child p').hide();
         $('div:nth-last-child(2) p').show();
+
     };
     
     if ($('#payment').val() == 'bitcoin'){
+        eraseCreditInfo()
         $('#credit-card').hide();
         $('div:nth-last-child(2) p').hide();
         $('div:last-child p').show();
     };
 });
 
+// Erase credit card info when another payment is selected
+const eraseCreditInfo = () => {
+    $('input', $('.credit-card')).each(function (){
+        $(this).val('').removeClass('redBorder').prev().removeClass('redText')
+    })
+}
+ 
 
 /*==============
 Register Button 
-================
-CHECK AND HIGHLIGHT ALL REQUIRED FIELDS */
+================*/
 
-
-// For styling purposes only (horizontal rule above 'Register' Button)
-$('button').wrap("<legend id='btn'></legend>");
-
-
+// Check and highlight all required fields
 $('button').on('click', (e) => {
-
-    // Name and email input fields
+    
+    // Name and email fields
     if ($('#name').val() == '') {
         $("#name").addClass('redBorder')
                 .attr('placeholder', 'Please enter a name...')
                 .prev().addClass('redText');
-        
     };
     if ($('#mail').val() == ''){
         $("#mail").addClass('redBorder')
                 .attr('placeholder', 'Please enter an email...')
                 .prev().addClass('redText');
-    }; 
-
-    // 'Job Role: other' field
-    if ($('#other-title').is(':not(:hidden)') && $('#other-title').val() == ''){
-        $("#other-title").addClass('redBorder').attr('placeholder', 'Please enter a job role...')
-    }
-
-    $('#other-title').on('change keypress', () => highlight('#other-title'));
-
-
-    // Loop through activities
+    };
+    
+  
+    // Loop through activities - At least one must be checked
     let empty = true
     $('input[type=checkbox]:checked').each(function () { $(this).is(':checked') ? empty = false : empty = true });
-    
-    if (empty){
-        $('.activities legend').first().addClass('redText').text('Please select at least one activity:')
-    } 
+    if (empty) $('.activities legend').first().addClass('redText').text('Please select at least one activity:');
 
-    // Credit card information
-    if ($('#cc-num').val() == '') $("#cc-num").addClass('redBorder').prev().addClass('redText');
-    if ($('#zip').val() == '') $("#zip").addClass('redBorder').prev().addClass('redText');
-    if ($('#cvv').val() == '') $("#cvv").addClass('redBorder').prev().addClass('redText');
-    
-    
-    /*=====================================================================================================================
-    I'M TRYING TO FIGURE OUT HOW TO LOOP THROUGH ALL THE INPUT FIELDS TO MAKE SURE THEY ARE FILLED OUT BEFORE I SUBMIT FORM
-    |                   |               |
-    |                   |               |
-    v                   v               v                                                                                  */
-    
-    $("input").each(function() {
-        
-        if ($(this).val() == ''){
-          
+
+    // Credit card information - Check for correct format
+    if ($('#payment').val() == 'credit card'){
+
+        if ($('#cc-num').val() == '') {
+            $("#cc-num").addClass('redBorder').prev().addClass('redText');
         }else{
+            let ccRegex = /^[0-9]{13,16}$/;
+            if (!ccRegex.test($('#cc-num').val())) $("#cc-num").addClass('redBorder').prev().addClass('redText');
+        };   
+
+        if ($('#zip').val() == '') {
+            $("#zip").addClass('redBorder').prev().addClass('redText');
+        }else{
+            let zipRegex = /^[0-9]{5}$/;
+            if (!zipRegex.test($('#zip').val())) $("#zip").addClass('redBorder').prev().addClass('redText');
+        };
+
+        if ($('#cvv').val() == '') {
+            $("#cvv").addClass('redBorder').prev().addClass('redText');
+        }else{
+            let ccvRegex = /^[0-9]{3}$/;
+            if (!ccvRegex.test($('#cvv').val())) $("#cvv").addClass('redBorder').prev().addClass('redText');
+        };
+    }
+    
+    // Submit form if there are no error messages
+    $('*').each(function () {
+        if ($(this).is('.redText, .redBorder')){
             e.preventDefault();
+            
         }
-                 
-    });
+    })
 });    
+
 
 // Reset input fields back to original styling once text is entered
 $('#name').on('change keypress', () => highlight('#name'))
@@ -190,30 +206,7 @@ $('#cc-num').on('change keypress', () => highlight('#cc-num'));
 $('#zip').on('change keypress', () => highlight('#zip'));
 $('#cvv').on('change keypress', () => highlight('#cvv'));
 
-
 const highlight = (field) => {
-    $(field).removeClass('redBorder', 'placeholder').prev().removeClass('redText')
-    if (event.which == 13) $(field).blur()
-}
-
-/*=================================
-CODE THAT WILL PROBABLY GET DELETED
-===================================*/
-
- // Remove 'Select Payment' option
-    // let payment = $('#payment').find("option").eq(0);
-    // if (payment.val() == 'select_method') payment.remove();
-
-
-// Create error message element if no payment is selected
-// $('<p id="select"></p>').insertBefore('#credit-card');
-
-// If no payment method is selected
-    // if ($('#payment').find("option").eq(0).val() == 'select_method'){
-    //     $('#select').show()
-    //     $('#select').text('Please select a payment method')
-    // }
-// $('#credit-card').hide();
-
- // Hide error message element
-    // $('#select').hide()
+    $(field).removeClass('redBorder', 'placeholder').prev().removeClass('redText');
+    if (event.which == 13) $(field).blur();
+};
